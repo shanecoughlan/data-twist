ce3497334171096dc5fa6b78d4a1d857f04f6d1b
Shane Martin Coughlan <shane@opendawn.com>
2013-03-05 14:11:05 +0900

    Release version 0.17
    This adds Japanese comments to the source code to make it easier for
    the cool people in Matsue to play with the code.

--- data-twist.rb
+++ data-twist.rb
@@ -8,14 +8,16 @@
 # You can redistribute it and/or modify it under either the terms of the
 # 2-clause BSDL (see the file BSDL), or the terms listed in README.md
 
+# required Ruby componants (必要なRubyのcomponants)
 require 'uri'
 require 'kconv'
 require 'Date'
 require 'optparse'
 require 'open-uri'
 
+# set the program name, version and copyright (プログラム名、バージョン、および著作権を設定する)
 $PROGRAM_NAME = 'Data Twist'
-$PROGRAM_VERSION = '0.16'
+$PROGRAM_VERSION = '0.17'
 $PROGRAM_COPYRIGHT = 'Copyright (c) 2013 Kana Fukuma and Shane Coughlan'
 $PROGRAM_COPYRIGHT_JA = '著作権 (c) 2013 福間加菜とコークラン クェーン マーティン'
 $PROGRAM_LICENSE = 'This application is licensed under Ruby + BSDL. See README.md for details.'
@@ -23,7 +25,7 @@ $PROGRAM_LICENSE_JA = 'このアプリケーションはルビー+ BSDL下でラ
 
 # input
 def input(inputfile)
-	# initialize the script
+	# initialize the script (スクリプトを初期化)
 	count = 0
 	array = []
 	id = 0
@@ -120,7 +122,7 @@ def input(inputfile)
 						#puts "duplication data:#{id},#{name},#{lat},#{lon}"
 						same_data = same_data + 1
 					end
-					# this is the progress feedback
+					# this is the progress feedback code (これは進行状況のフィードバック·コードです)
 					(write_data + same_data + 1).times do |processed|
 					print "\rLocations processed so far: #{processed}"
 					end
@@ -141,6 +143,7 @@ def input(inputfile)
 				
 			end
 		}
+	#  this was commented out because it caused an error loop? Please review. (それはエラーループが発生したため、これはコメントアウトされたのですか？レビューをしてください。)
 	#rescue
 	#	print "error"
 	#end
@@ -152,9 +155,9 @@ def input(inputfile)
 	return array,term_count
 end
 
-# copy sql_format
+# gets the sql format information (SQL形式の情報を取得します)
 def copy_format(new_file)
-	filename = "sql_format.sql" # the sql format information
+	filename = "sql_format.sql" # Sets the SQL format filename (SQL形式のファイル名を設定します)
 	last_str = []
 	num = 0
 
@@ -162,10 +165,10 @@ def copy_format(new_file)
 	file = File.read(filename)
 	File.open(new_file,"w"){ |output|
 		file.each_line { |input|
-			if input.include?("=@OLD") # catches the last string
+			if input.include?("=@OLD") # catches the last string (最後の文字列をキャッチ)
 				last_str << input
 			else
-				output.write input # writes the output file
+				output.write input # writes the output file (出力ファイルを書き込み)
 			end
 		}
 	}
@@ -179,7 +182,7 @@ def output(last_str,outputfile,array,term_count)
 	content = "<br/>[geo_mashup_map zoom=16]<br/><small>Geo-data from OpenStreetMap. &copy; OpenStreetMap contributors. OpenStreetMap is <i>open data</i>, licensed under the <a href=\"http://opendatacommons.org/licenses/odbl/\">Open Data Commons Open Database License</a> (ODbL)</small><br/>This entry was last updated: "
 	File.open(outputfile,"a") { |f|
 		
-		# write about the "wp_posts" file
+		# write about the "wp_posts" file ("wp_posts"ファイルについて書く)
 		f.write "--\n-- Dumping data for table `wp_posts`\n--\n\n"
 		f.write "INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES\n"
 
@@ -192,7 +195,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 		
 		count = 0
-		# write about the "wp_geo_mashup_locations" file
+		# write about the "wp_geo_mashup_locations" file ("wp_geo_mashup_locations"ファイルについて書く)
 		f.write "--\n-- Dumping data for table `wp_geo_mashup_locations`\n--\n\n"
 		f.write "INSERT INTO `wp_geo_mashup_locations` (`id`, `lat`, `lng`, `address`, `saved_name`, `geoname`, `postal_code`, `country_code`, `admin_code`, `sub_admin_code`, `locality_name`) VALUES\n"
 		
@@ -205,7 +208,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 
 		count = 0
-		# write about the "wp_geo_mashup_location_relationships" file
+		# write about the "wp_geo_mashup_location_relationships" file ("wp_geo_mashup_location_relationships"ファイルについて書く)
 		f.write "--\n-- Dumping data for table `wp_geo_mashup_location_relationships`\n--\n\n"
 		f.write "INSERT INTO `wp_geo_mashup_location_relationships` (`object_name`, `object_id`, `location_id`, `geo_date`) VALUES\n"
 		
@@ -218,7 +221,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 
 		count = 0
-		# write about the "wp_term_relationships" file
+		# write about the "wp_term_relationships" file ("wp_term_relationships"ファイルについて書く)
 		f.write "--\n-- Dumping data for table `wp_term_relationships`\n--\n\n"
 		f.write "INSERT INTO `wp_term_relationships` (`object_id`, `term_taxonomy_id`, `term_order`) VALUES\n"
 		
@@ -231,7 +234,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 
 		count = 0
-		# write about the wp_term_taxonomy
+		# write about the wp_term_taxonomy (wp_term_taxonomyについて書く)
 		f.write "--\n-- Dumping data for table `wp_term_taxonomy`\n--\n\n"
 		f.write "INSERT INTO `wp_term_taxonomy` (`term_taxonomy_id`, `term_id`, `taxonomy`, `description`, `parent`, `count`) VALUES\n"
 		
@@ -239,7 +242,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write "(4,4,'category','amenity',0,#{term_count[1]});\n\n"
 
 
-		# write the last string
+		# write the last string (最後の文字列を書き込む)
 		last_str.each { |str|
 			f.write str
 		}
@@ -253,22 +256,18 @@ def check_same_place(array)
 	
 end
 
-# check the command line for options
+# check the command line for options (オプションについては、コマンドラインを確認)
 options = {}
 QA = OptionParser.new do |opts|
 	
-  # prepares an overview banner for the --help switch
+  # prepares an overview banner for the --help switch (helpスイッチの概要バナーを準備)
 	opts.banner = "\nUsage overview (使用方法の概要): data-twist.rb [options]"
 	
   # gets a file
-  	options[:load_file] = "" # This is pending review as we expand the code
+  	options[:load_file] = "" # code to load a file based on command line input (ファイルをロードするためのコードでは、コマンドラインの入力に基づいて、)
 		opts.on( '-g', '--get FILE', 'Get file (ファイルを取得)' ) do|urls|
-		# this code segment is based on code from here:
+		# code below is from here (以下のコードは、ここからです):
 		# http://www.ruby-forum.com/topic/92121#183390
-		# Edwin Fine, based on Mariano Kamp's original
-		# urls = %w{
-		# http://download.geofabrik.de/openstreetmap/asia/tajikistan.osm.bz2
-		# } This is legacy from the old code, but keeping it while I review the next step
 
 		BUFFER_SIZE = 8 * 1_024
 
@@ -288,25 +287,24 @@ QA = OptionParser.new do |opts|
 		  end
 		  puts "\nOK, the download is complete."
 		end
-		# puts "All downloads done.", a legacy bit of text from the original code
 		puts "Please unzip the downloaded file before loading it into Data Twist."
 		puts "You can get help by typing 'ruby data-twist.rb -h'"
 		exit
 	end
     
-  # takes the input file name
+  # takes the input file name (入力ファイル名を受け取り、)
 	options[:load_file] = ""
 		opts.on( '-l', '--load FILE', 'Load file (ファイルを読み込む)' ) do|load_file|
 		options[:load_file] = load_file
 	end
 
-  # takes the output file name
+  # takes the output file name (出力ファイル名を受け取り、)
 	options[:output_file] = ""
 		opts.on( '-o', '--output FILE', 'Create output file (出力ファイル名)' ) do|output_file|
 		options[:output_file] = output_file
 	end 
    
-  # displays the help screen
+  # displays the help screen (ヘルプ画面が表示されます)
 	opts.on( '-h', '--help', 'Show this overview (この概要を表示)' ) do
 		puts opts
 		puts   ""
@@ -319,7 +317,7 @@ QA = OptionParser.new do |opts|
 		exit
 	end
 	
-  # shows the version, copyright and license
+  # shows the version, copyright and license (示しバージョン、著作権とライセンス)
 	opts.on( '-v', '--version', 'Show the version (バージョンを表示)' ) do
 		puts "\nYou are using #{$PROGRAM_NAME} #{$PROGRAM_VERSION}"
 		puts "あなたは、#{$PROGRAM_NAME} #{$PROGRAM_VERSION}を使用している"
@@ -332,7 +330,7 @@ QA = OptionParser.new do |opts|
 	
 end.parse!
 
-  # check to deal with a missing input file name
+  # check to deal with a missing input file name (不足している入力ファイル名に対処するためにチェック)
 	if options[:load_file].empty?
 		then
 		puts "\nI cannot see the input file. Please try again."
@@ -341,13 +339,15 @@ end.parse!
 		exit
 	end
 		
-  # check to insert a default output file name if needed
+  # check to insert a default output file name if needed (必要に応じて、デフォルトの出力ファイル名を挿入するにチェック)
 	if options[:output_file].empty?
 		then options[:output_file] = "output.sql"
 	end
 
-inputfile = "#{options[:load_file]}" # this takes values from options[:load_file]
-outputfile = "#{options[:output_file]}" # this takes values from options[:output_file]
+# next line takes values from (次の行では、から値を取得): options[:load_file]
+inputfile = "#{options[:load_file]}"
+# next line takes values from (次の行では、から値を取得): options[:output_file]
+outputfile = "#{options[:output_file]}" 
 array,term_count = input(inputfile)
 last_str = copy_format(outputfile)
 output(last_str,outputfile,array,term_count)
