52342acf735280de2103c5ff2c6a7a11c6fa6405
Kana Fukuma <shane@opendawn.com>
2013-03-14 15:22:46 +0900

    Released version 0.24
    Improved Japanese comments in the script
    (スクリプトで改善された日本語のコメント)
    
    Fixed a bug with displaying UNCATEGORIZED posts
    (未分類の記事を表示する際のバグを修正)

--- data-twist.rb
+++ data-twist.rb
@@ -17,7 +17,7 @@ require 'open-uri'
 
 # set the program name, version and copyright (プログラム名、バージョン、および著作権を設定する)
 $PROGRAM_NAME = 'Data Twist'
-$PROGRAM_VERSION = '0.23'
+$PROGRAM_VERSION = '0.24'
 $PROGRAM_COPYRIGHT = 'Copyright (c) 2013 Kana Fukuma and Shane Coughlan'
 $PROGRAM_COPYRIGHT_JA = '著作権 (c) 2013 福間加菜とコークラン クェーン マーティン'
 $PROGRAM_LICENSE = 'This application is licensed under Ruby + BSDL. See README.md for details.'
@@ -42,7 +42,7 @@ def input(inputfile)
 	write_data = 0
 	all = 0
 	term = 0
-	term_count = [0,0] # [0] = shop_count, [1] = amenity_count
+	term_count = [0,0,0] # [0] = shop_count, [1] = amenity_count, [2] = uncategorized
 
 	# filename input
 	begin
@@ -50,8 +50,8 @@ def input(inputfile)
 		file = File.read(filename)
 	end
 	
-	# output to explain data twisting has started (データのねじれを説明するために出力を開始しました)
-	print "\n== Starting to twist data / データをねじら開始 ==\n\n"
+	# output to explain data twisting has started (データツイストを開始する説明を出力)
+	print "\n== Starting to twist data / データツイストの開始 ==\n\n"
 	
 #	begin
 		# read a line from the file
@@ -88,12 +88,10 @@ def input(inputfile)
 				if line.match("k=\"shop\"")
 					if /v="(.*)"/ =~ line
 						shop = $1
-						term_count[0] = term_count[0]+1
 					end
 				elsif line.match("k=\"amenity\"")
 					if /v="(.*)"/ =~ line
 						amenity = $1
-						term_count[1] = term_count[1]+1
 					end
 				end
 			end
@@ -106,27 +104,30 @@ def input(inputfile)
 					e_name = URI.escape(e_name)
 					timestamp.gsub!("T", " ")
 					timestamp.gsub!("Z", " ")
-					if shop != ""
+					
+					if shop != "" # shop
 						desc = shop
 						term = 3
-					elsif amenity != ""
+						term_count[0] = term_count[0]+1
+					elsif amenity != "" # amenity
 						desc = amenity
 						term = 4
-					else
-						term = 1
+						term_count[1] = term_count[1]+1
+					else # uncategorized
+						term = 5
+						term_count[2] = term_count[2]+1
 					end
-					# this is the checking code to find duplicate latitude and longitude (これは、重複する緯度と経度を見つけるためのチェックコードである)
+					# this is the checking code to find duplicate latitude and longitude (重複する緯度・経度を見つけるためのチェックコード)
 					if (a = array.select{ |a| a[4] == lat && a[5] == lon}) == []
 						array << [type,name,desc,id,lat,lon,e_name,timestamp,term]
 						write_data = write_data + 1
 					else
-						#p a
 						#puts "duplication data:#{id},#{name},#{lat},#{lon}"
 						same_data = same_data + 1
 					end
-					# this is the progress feedback code (これは進行状況のフィードバック·コードです)
+					# this is the progress feedback code (進行状況のフィードバックコード)
 					(write_data + same_data + 1).times do |processed|
-					print "\rLocations processed so far (場所はこれまでに処理): #{processed}"
+					print "\rLocations processed so far (Locationはこれまでに処理): #{processed}"
 					end
 				end
 
@@ -142,17 +143,16 @@ def input(inputfile)
 				type = ""
 				timestamp = ""
 				flag = false
-				
 			end
 		}
 
 	print "\n\n== Summary / 要約 ==\n"
 	puts "\nI found #{same_data} duplicate entries in the input file."
-	puts "私は#{same_data}は、入力ファイル内に重複したエントリを見つけました。"
+	puts "入力ファイル内に#{same_data}個の重複したエントリを見つけました。"
 	puts "\nI wrote #{write_data} locations to the output file."
-	puts "私は出力ファイルに#{write_data}場所を書いた。"
+	puts "出力ファイルに#{write_data}か所の情報を書きました。"
 	puts "\nI processed a total of #{same_data + write_data} locations during my analysis."
-	puts "私は、分析中に#{same_data + write_data}場所の合計を処理しました。"
+	puts "分析中に合計#{same_data + write_data}個のエントリを処理しました。"
 	puts "\n"
 	return array,term_count
 end
@@ -184,7 +184,7 @@ def output(last_str,outputfile,array,term_count)
 	content = "<br/>[geo_mashup_map zoom=16]<br/><small>Geo-data from OpenStreetMap. &copy; OpenStreetMap contributors. OpenStreetMap is <i>open data</i>, licensed under the <a href=\"http://opendatacommons.org/licenses/odbl/\">Open Data Commons Open Database License</a> (ODbL)</small><br/>This entry was last updated: "
 	File.open(outputfile,"a") { |f|
 		
-		# write about the "wp_posts" file ("wp_posts"ファイルについて書く)
+		# write about the "wp_posts" file ("wp_posts"ファイルについて書きます)
 		f.write "--\n-- Dumping data for table `wp_posts`\n--\n\n"
 		f.write "INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES\n"
 
@@ -197,7 +197,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 		
 		count = 0
-		# write about the "wp_geo_mashup_locations" file ("wp_geo_mashup_locations"ファイルについて書く)
+		# write about the "wp_geo_mashup_locations" file ("wp_geo_mashup_locations"ファイルについて書きます)
 		f.write "--\n-- Dumping data for table `wp_geo_mashup_locations`\n--\n\n"
 		f.write "INSERT INTO `wp_geo_mashup_locations` (`id`, `lat`, `lng`, `address`, `saved_name`, `geoname`, `postal_code`, `country_code`, `admin_code`, `sub_admin_code`, `locality_name`) VALUES\n"
 		
@@ -210,7 +210,7 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 
 		count = 0
-		# write about the "wp_geo_mashup_location_relationships" file ("wp_geo_mashup_location_relationships"ファイルについて書く)
+		# write about the "wp_geo_mashup_location_relationships" file ("wp_geo_mashup_location_relationships"ファイルについて書きます)
 		f.write "--\n-- Dumping data for table `wp_geo_mashup_location_relationships`\n--\n\n"
 		f.write "INSERT INTO `wp_geo_mashup_location_relationships` (`object_name`, `object_id`, `location_id`, `geo_date`) VALUES\n"
 		
@@ -223,23 +223,22 @@ def output(last_str,outputfile,array,term_count)
 		f.write ";\n\n"
 
 		count = 0
-		# write about the "wp_term_relationships" file ("wp_term_relationships"ファイルについて書く)
+		# write about the "wp_term_relationships" file ("wp_term_relationships"ファイルについて書きます)
 		f.write "--\n-- Dumping data for table `wp_term_relationships`\n--\n\n"
 		f.write "INSERT INTO `wp_term_relationships` (`object_id`, `term_taxonomy_id`, `term_order`) VALUES\n"
 		
 		array.each { |a|
 			count = count + 1
 			f.write "(#{a[3]}, #{a[8]}, 0)"
-			
 			f.write ",\n" if array.length != count
 		}
 		f.write ";\n\n"
 
 		count = 0
-		# write about the wp_term_taxonomy (wp_term_taxonomyについて書く)
+		# write about the wp_term_taxonomy (wp_term_taxonomyについて書きます)
 		f.write "--\n-- Dumping data for table `wp_term_taxonomy`\n--\n\n"
 		f.write "INSERT INTO `wp_term_taxonomy` (`term_taxonomy_id`, `term_id`, `taxonomy`, `description`, `parent`, `count`) VALUES\n"
-		
+		f.write "(5,1,'category','uncategorized',0,#{term_count[2]}),\n"
 		f.write "(3,3,'category','shop',0,#{term_count[0]}),\n"
 		f.write "(4,4,'category','amenity',0,#{term_count[1]});\n\n"
 
@@ -259,16 +258,16 @@ QA = OptionParser.new do |opts|
 	opts.banner = "\nUsage overview (使用方法の概要): data-twist.rb [options]"
 	
   # gets a file
-  	options[:get_file] = "" # code to get a file based on command line input (ファイルをロードするためのコードでは、コマンドラインの入力に基づいて、)
+  	options[:get_file] = "" # code to get a file based on command line input (コマンドラインの入力に基づいて、ファイルを取得するためのコード)
 		opts.on( '-g', '--get FILE', 'Get file (ファイルを取得)' ) do|get_file|
 
 		# sets a buffer size (バッファサイズを設定します)
 		BUFFER_SIZE = 8 * 1_024
 
-		# gets the file and names it according to the end of the URL (ファイルを取得し、URLの末尾に応じてそれに名前を付け)
-		puts "\nDownloading from (からのダウンロード) #{get_file}"
+		# gets the file and names it according to the end of the URL (ファイルを取得し、URLの末尾に応じてそれに名前を付ける)
+		puts "\nDownloading from (ダウンロード) #{get_file}"
 		downloaded_file = get_file.split(/\//).last
-		# output to the user to confirm this (ユーザへの出力このことを確認するための)
+		# output to the user to confirm this (これを確認するためのユーザへの出力)
 		puts "Creating (作成): #{downloaded_file}"
 
 		# checks remote file size and shows download progress (リモート·ファイル·サイズをチェックして、ダウンロードの進行状況を示しています)
@@ -293,27 +292,27 @@ QA = OptionParser.new do |opts|
 		puts "ダウンロードが完了しました。"
 	end
     
-  # takes the input file name (入力ファイル名を受け取り、)
+  # takes the input file name (入力ファイル名を受け取る)
 	options[:load_file] = ""
 		opts.on( '-l', '--load FILE', 'Load file (ファイルを読み込む)' ) do|load_file|
 		options[:load_file] = load_file
 	end
 
-  # takes the output file name (出力ファイル名を受け取り、)
+  # takes the output file name (出力ファイル名を受け取る)
 	options[:output_file] = ""
 		opts.on( '-o', '--output FILE', 'Create output file (出力ファイル名)' ) do|output_file|
 		options[:output_file] = output_file
 	end 
    
-  # displays the help screen (ヘルプ画面が表示されます)
-	opts.on( '-h', '--help', 'Show this overview (この概要を表示)' ) do
+  # displays the help screen (ヘルプ画面を表示)
+	opts.on( '-h', '--help', 'Show this overview (概要を表示)' ) do
 		puts opts
 		puts   ""
 		puts   "Example commands (コマンドの例):"
 		puts   "\nGet a file with 'ruby data-twist.rb -g URL'"
 		puts   "'ruby data-twist.rb -g URL'を使用してファイルを取得する"
 		puts   "\nConvert data with 'ruby data-twist.rb -l [INPUT FILE] -o [OUTPUT]'"
-		puts   "'ruby data-twist.rb -l [INPUT FILE] -o [OUTPUT]'を使用してデータを変換します"
+		puts   "'ruby data-twist.rb -l [INPUT FILE] -o [OUTPUT]'を使用してデータを変換する"
 		puts   ""
 		exit
 	end
@@ -346,14 +345,15 @@ end.parse!
 			end
 	end
 		
-  # check to insert a default output file name if needed (必要に応じて、デフォルトの出力ファイル名を挿入するにチェック)
+  # check to insert a default output file name if needed (必要に応じて、デフォルトの出力ファイル名を挿入することをチェック)
 	if options[:output_file].empty?
 		then options[:output_file] = "output.sql"
 	end
 
-# next line takes values from (次の行では、から値を取得): options[:load_file]
+# next line takes values from (次の行では、次の値を取得): options[:load_file]
 inputfile = "#{options[:load_file]}"
-# next line takes values from (次の行では、から値を取得): options[:output_file]
+
+# next line takes values from (次の行では、次の値を取得): options[:output_file]
 outputfile = "#{options[:output_file]}" 
 array,term_count = input(inputfile)
 last_str = copy_format(outputfile)
